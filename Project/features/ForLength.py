51741be5884b6eb7d1db03b1e50147f8f8c6da7c
Daniel Strokovsky <dstrokovsky@hotmail.com>
2014-10-31 19:49:49 +0200

    Fixed no loops problem.

--- Project/features/ForLength.py
+++ Project/features/ForLength.py
@@ -2,4 +2,6 @@ from Project.StringExtractor import forExtract
 
 def forMaxLength(s):
     fors = forExtract(s)
+    if fors==[]:
+        return 0
     return len(max(fors, key=len))
--- Project/features/WhileLength.py
+++ Project/features/WhileLength.py
@@ -2,4 +2,6 @@ from Project.StringExtractor import whileExtract
 
 def whileMaxLength(s):
     loops = whileExtract(s)
+    if loops == []:
+        return 0
     return len(max(loops, key=len))
