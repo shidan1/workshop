d413610d822fbc952a9e8fbef0b402a3ccde5555
Daniel Strokovsky <dstrokovsky@hotmail.com>
2014-10-31 22:10:45 +0200

    Updated the word size metric.

--- Project/features/WordSize.py
+++ Project/features/WordSize.py
@@ -1,2 +1,11 @@
-def wordSize(s):
+def maxWordSize(s):
     return len(max(s.split(), key=len))
+    
+def avgWordSize(s):
+    words = s.split()
+    return sum(len(word) for word in words)/len(words)
+
+
+
+    
+    
